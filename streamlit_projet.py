71ef59ea1ff4006851ee12a861b4a41f63068bfa
Nathalie <nathalie.albora@gmail.com>
2025-01-16 14:13:19 +0100

    Update streamlit_projet.py

--- streamlit_projet.py
+++ streamlit_projet.py
@@ -305,6 +305,7 @@ def afficher_films(categorie, annee=None):
                 if clicked:
                     st.session_state.selected_film = film_title
                     st.session_state.page = "details"
+                    st.rerun()
 
             # Ajouter titre du film en dessous de l'image
                 st.markdown(f"""
